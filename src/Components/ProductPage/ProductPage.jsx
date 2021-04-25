7b741e7f5b06f5f041e44ee54b885c6b8b021687
kunal7722 <dev35.programmingtree@gmail.com>
2021-04-25 22:11:54 +0530

    Refactor - Extract getSortedData and getFilteredData in a seprate file

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -34,20 +34,6 @@ export const ProductPage = () => {
     );
   }, [searchTerm]);
 
-  // const getsortedData = (productData, sortBy) => {
-  //   if (sortBy && sortBy === "PRICE_LOW_TO_HIGH") {
-  //     return productData.sort((a, b) => a["price"] - b["price"]);
-  //   }
-  //   if (sortBy && sortBy === "PRICE_HIGH_TO_LOW") {
-  //     return productData.sort((a, b) => b["price"] - a["price"]);
-  //   }
-  //   return productData;
-  // };
-  // const getFilteredData = (productData, { fastDeliveryOnly, inventoryAll }) => {
-  //   return productData
-  //     .filter(({ fastDelivery }) => (fastDeliveryOnly ? fastDelivery : true))
-  //     .filter(({ inStock }) => (inventoryAll ? true : inStock));
-  // };
   const sortedData = getSortedData(searchResults, sortBy);
   const filteredData = getFilteredData(sortedData, {
     fastDeliveryOnly,
